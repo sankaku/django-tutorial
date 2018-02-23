76b05b570cb9cb0a87f8ecac933ebedca3316ea1
sankaku <sankaku.git@gmail.com>
2018-02-23 18:28:51 +0900

    add another views(detail, results, vote)

--- app/polls/views.py
+++ app/polls/views.py
@@ -2,5 +2,20 @@
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def index(request):
     return HttpResponse('Hello, polls.')
+
+
+def detail(request, question_id):
+    return HttpResponse('[detail] question_id = %s' % question_id)
+
+
+def results(request, question_id):
+    response = '[results] question_id = %s'
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse('[vote] question_id = %s' % question_id)
