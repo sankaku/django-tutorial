063a34f789ce1df10ac51265217031182ec66e98
sankaku <sankaku.git@gmail.com>
2018-02-25 17:21:34 +0900

    mod hardcoded href

--- app/polls/urls.py
+++ app/polls/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('index_by_render/', views.index_by_render, name='index_by_render'),
