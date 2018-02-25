d3bfa6396dc8f5e46d53dd5f1ac9bc56625ddcf1
sankaku <sankaku.git@gmail.com>
2018-02-25 18:59:30 +0900

    mod by generic view

--- app/polls/views.py
+++ app/polls/views.py
@@ -3,8 +3,27 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Question, Choice
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
 # Create your views here.
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def index(request):
