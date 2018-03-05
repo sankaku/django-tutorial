fcd6b4326d889969dea82b8578c8e458d15e73d7
sankaku <sankaku.git@gmail.com>
2018-03-05 15:05:38 +0900

    customize Question page in admin
    1. change items order
      question_text, pub_date -> pub_date, question_text
    2. add headings to question_text and pub_date

--- app/polls/admin.py
+++ app/polls/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from .models import Question
 
 # Register your models here.
-admin.site.register(Question)
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text']
+
+    fieldsets = [
+        ('Date Information', {'fields': ['pub_date']}),
+        ('Text Information', {'fields': ['question_text']}),
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
